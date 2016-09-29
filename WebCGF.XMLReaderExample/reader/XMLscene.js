da68f880e922fd0d45926a349082a7ab134bf14a
Jose Martins <lluismmartins7@gmail.com>
2016-09-29 19:57:27 +0100

    Resolved problem with camera

--- WebCGF.XMLReaderExample/reader/XMLscene.js
+++ WebCGF.XMLReaderExample/reader/XMLscene.js
@@ -1,6 +1,8 @@
 
-function XMLscene() {
+function XMLscene(myInterface) {
     CGFscene.call(this);
+
+    this.interface=myInterface;
 }
 
 XMLscene.prototype = Object.create(CGFscene.prototype);
@@ -17,7 +19,7 @@ XMLscene.prototype.init = function (application) {
 
     this.gl.clearDepth(100.0);
     this.gl.enable(this.gl.DEPTH_TEST);
-	this.gl.enable(this.gl.CULL_FACE);
+	  this.gl.enable(this.gl.CULL_FACE);
     this.gl.depthFunc(this.gl.LEQUAL);
 
 	this.axis=new CGFaxis(this);
@@ -38,30 +40,31 @@ XMLscene.prototype.setDefaultAppearance = function () {
     this.setAmbient(0.2, 0.4, 0.8, 1.0);
     this.setDiffuse(0.2, 0.4, 0.8, 1.0);
     this.setSpecular(0.2, 0.4, 0.8, 1.0);
-    this.setShininess(10.0);	
+    this.setShininess(10.0);
 };
 
-// Handler called when the graph is finally loaded. 
+// Handler called when the graph is finally loaded.
 // As loading is asynchronous, this may be called already after the application has started the run loop
-XMLscene.prototype.onGraphLoaded = function () 
+XMLscene.prototype.onGraphLoaded = function ()
 {
 	//this.gl.clearColor(this.graph.background[0],this.graph.background[1],this.graph.background[2],this.graph.background[3]);
 	this.gl.clearColor(0,0,0,1);
 	this.lights[0].setVisible(true);
-    this.lights[0].enable();
+  this.lights[0].enable();
 	this.initGraphCameras();
 };
 
 
 XMLscene.prototype.initGraphCameras = function () {
-    this.camera = new CGFcamera(this.graph.angle, this.graph.near, this.graph.far, 
+    this.camera = new CGFcamera(this.graph.angle, this.graph.near, this.graph.far,
 		vec3.fromValues(this.graph.x1, this.graph.y1, this.graph.z1), vec3.fromValues(this.graph.x2, this.graph.y2, this.graph.z2));
+    this.interface.setActiveCamera(this.camera);
 };
 
 
 XMLscene.prototype.display = function () {
 	// ---- BEGIN Background, camera and axis setup
-	
+
 	// Clear image and depth buffer everytime we update the scene
     this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
     this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
@@ -77,7 +80,7 @@ XMLscene.prototype.display = function () {
 	this.axis.display();
 
 	this.setDefaultAppearance();
-	
+
 	// ---- END Background, camera and axis setup
 
 	// it is important that things depending on the proper loading of the graph
@@ -86,6 +89,5 @@ XMLscene.prototype.display = function () {
 	if (this.graph.loadedOk)
 	{
 		this.lights[0].update();
-	};	
+	};
 };
-
