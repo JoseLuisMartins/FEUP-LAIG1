3d9591186322eedade155827ac9197407fcde91c
Jose Martins <lluismmartins7@gmail.com>
2016-09-29 18:05:42 +0100

    test commit

--- WebCGF.XMLReaderExample/reader/MySceneGraph.js
+++ WebCGF.XMLReaderExample/reader/MySceneGraph.js
@@ -1,12 +1,12 @@
 
 function MySceneGraph(filename, scene) {
 	this.loadedOk = null;
-	
+
 	// Establish bidirectional references between scene and graph
 	this.scene = scene;
 	scene.graph=this;
-		
-	// File reading 
+
+	// File reading
 	this.reader = new CGFXMLreader();
 
 	/*
@@ -14,28 +14,28 @@ function MySceneGraph(filename, scene) {
 	 * After the file is read, the reader calls onXMLReady on this object.
 	 * If any error occurs, the reader calls onXMLError on this object, with an error message
 	 */
-	 
-	this.reader.open('scenes/'+filename, this);  
+
+	this.reader.open('scenes/'+filename, this);
 }
 
 /*
  * Callback to be executed after successful reading
  */
-MySceneGraph.prototype.onXMLReady=function() 
+MySceneGraph.prototype.onXMLReady=function()
 {
 	console.log("XML Loading finished.");
 	var rootElement = this.reader.xmlDoc.documentElement;
-	
+
 	// Here should go the calls for different functions to parse the various blocks
 	var error = this.loadViews(rootElement);
 
 	if (error != null) {
 		this.onXMLError(error);
 		return;
-	}	
+	}
 
 	this.loadedOk=true;
-	
+
 	// As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
 	this.scene.onGraphLoaded();
 };
@@ -46,7 +46,7 @@ MySceneGraph.prototype.onXMLReady=function()
  * Example of method that parses elements of one block and stores information in a specific data structure
  */
 MySceneGraph.prototype.parseGlobalsExample= function(rootElement) {
-	
+
 	/*
 	var elems =  rootElement.getElementsByTagName('scene');
 	if (elems == null) {
@@ -71,7 +71,7 @@ MySceneGraph.prototype.parseGlobalsExample= function(rootElement) {
 	if (tempList == null  || tempList.length==0) {
 		return "list element is missing.";
 	}
-	
+
 	this.list=[];
 	// iterate over every element
 	var nnodes=tempList[0].children.length;
@@ -88,35 +88,35 @@ MySceneGraph.prototype.parseGlobalsExample= function(rootElement) {
 
 
 MySceneGraph.prototype.loadScene= function(rootElement) {
-	
+
 	var elems = rootElement.getElementsByTagName('scene');
 }
 
 MySceneGraph.prototype.loadViews= function(rootElement) {
-	
+
 	var elems = rootElement.getElementsByTagName('views');
 	if (elems == null)
 		onXMLError("Error reading views");
-	
+
 	this.view = elems[0];
 	if (this.view == null)
 		onXMLError("Error loading view.");
-	
+
 	this.perspective = this.view.getElementsByTagName('perspective')[0];
 	this.near = this.reader.getFloat(this.perspective, 'near');
 	this.far = this.reader.getFloat(this.perspective, 'far');
 	this.angle = this.reader.getFloat(this.perspective, 'angle');
-	
+
 	this.from = this.perspective.getElementsByTagName('from')[0];
 	if (this.from == null)
 		onXMLError("Error loading 'from'.");
 	/*
 	var children = this.perspective.children;
-	
+
 	this.x1 = children[0].attributes.getNamedItem("x").value;
 	this.y1 = children[0].attributes.getNamedItem("y").value;
 	this.z1 = children[0].attributes.getNamedItem("z").value;
-	
+
 	this.x2 = children[1].attributes.getNamedItem("x").value;
 	this.y2 = children[1].attributes.getNamedItem("y").value;
 	this.z2 = children[1].attributes.getNamedItem("z").value;
@@ -126,21 +126,19 @@ MySceneGraph.prototype.loadViews= function(rootElement) {
 	this.x1 = this.reader.getFloat(this.from, 'x');
 	this.y1 = this.reader.getFloat(this.from, 'y');
 	this.z1 = this.reader.getFloat(this.from, 'z');
-	
+
 	this.to = this.perspective.getElementsByTagName('to')[0];
 	this.x2 = this.reader.getFloat(this.to, 'x');
 	this.y2 = this.reader.getFloat(this.to, 'y');
 	this.z2 = this.reader.getFloat(this.to, 'z');
 }
 
-	
+
 /*
  * Callback to be executed on any read error
  */
- 
+
 MySceneGraph.prototype.onXMLError=function (message) {
-	console.error("XML Loading Error: "+message);	
+	console.error("XML Loading Error: "+message);
 	this.loadedOk=false;
 };
-
-
